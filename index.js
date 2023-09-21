0845b9f051805342336a3c1cf09a2eb961b20b99
Seol <131204758+JG-0204@users.noreply.github.com>
2023-09-21 10:50:30 +0800

    Add createCard and displayBook function
    createCard takes the user input and a container element, then appends it to the body. displayBook just loops through the array and checked if the array object isDisplayed is false if it is then it will call the createCard function and if is true it just skips the object

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 // Storage
-const library = [
-  {
-    title: 'Midnight Library',
-    author: 'Matt Haig',
-    pages: 210,
-    isRead: true,
-  },
-];
+const library = [];
 
 // Book object constructor
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead ?? false;
+  this.isRead = isRead == 'true' ? true : false;
 }
-
-let atomic = new Book('Atomic Habits', 'James Clear', 214);
 
 function inputBook() {
   let title = prompt('book title', 'atomic');
@@ -27,8 +18,43 @@ function inputBook() {
   let book = new Book(title, author, pages, isRead);
 
   library.push(book);
+
+  displayBook();
 }
 
 const addButton = document
   .querySelector('.add-book')
   .addEventListener('click', inputBook);
+
+function displayBook() {
+  const container = document.querySelector('.cards-container');
+  library.forEach((book) => {
+    if (!book.isDisplayed) {
+      book.isDisplayed = true;
+      console.log(book);
+      createCard(book.title, book.author, book.pages, book.isRead, container);
+    }
+  });
+}
+
+function createCard(title, author, pages, status, container) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const bookTitle = document.createElement('p');
+  bookTitle.classList.add('title');
+  const bookAuthor = document.createElement('p');
+  bookAuthor.classList.add('author');
+  const bookPages = document.createElement('p');
+  bookPages.classList.add('pages');
+  const bookStatus = document.createElement('p');
+  bookStatus.classList.add('status');
+
+  bookTitle.textContent = title;
+  bookAuthor.textContent = author;
+  bookPages.textContent = pages;
+  bookStatus.textContent = status ? 'Read' : 'Unread';
+
+  card.append(bookTitle, bookAuthor, bookPages, bookStatus);
+  container.appendChild(card);
+}
