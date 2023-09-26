f380e1600c1e2b109ea7d74f9e3dbdf5321fb71b
Seol <131204758+JG-0204@users.noreply.github.com>
2023-09-26 11:40:11 +0800

    Change constructor to Class
    Add Book Class which create a new book object and Library Class that creates an array for Books and has methods like addBook, removeBook, changeBookStatus and displayBook. Change some functions to make it work with classes

--- index.js
+++ index.js
@@ -3,21 +3,40 @@ const bookContainer = document.querySelector('.cards-container');
 
 const addBookModal = document.querySelector('dialog');
 const form = document.querySelector('form');
-const titleInput = document.querySelector('#book-title');
-const authorInput = document.querySelector('#book-author');
-const pagesInput = document.querySelector('#book-pages');
-const statusInput = document.querySelector('#book-status');
 
-// Storage
-const library = [];
-
-// Book object constructor
-function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = status;
+class Book {
+  constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
 }
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  removeBook(book) {
+    const bookIndex = this.books.indexOf(book);
+    this.books.splice(bookIndex, 1);
+  }
+
+  displayBook(book) {
+    createCard(book);
+  }
+
+  changeBookStatus(book) {
+    book.status = book.status ? false : true;
+  }
+}
+
+const library = new Library();
 
 const showModalButton = document.querySelector('.input-book');
 showModalButton.addEventListener('click', () => {
@@ -26,37 +45,25 @@ showModalButton.addEventListener('click', () => {
 
 const addBookButton = document.querySelector('.add-book');
 addBookButton.addEventListener('click', (e) => {
-  const title = titleInput.value;
-  const author = authorInput.value;
-  const pages = pagesInput.value;
-  const status = statusInput.checked;
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#book-author').value;
+  const pages = document.querySelector('#book-pages').value;
+  const status = document.querySelector('#book-status').checked;
 
   // check if form is valid
   if (form.checkValidity()) {
     e.preventDefault();
 
-    addBookToLibrary(title, author, pages, status);
+    const book = new Book(title, author, pages, status);
+    library.addBook(book);
     addBookModal.close();
-    displayBook();
+
+    library.displayBook(book);
+
     // reset form values
     form.reset();
   }
 });
-
-function addBookToLibrary(title, author, pages, status) {
-  const book = new Book(title, author, pages, status);
-  library.push(book);
-}
-
-function displayBook() {
-  library.forEach((book) => {
-    if (!book.isDisplayed) {
-      createCard(book);
-
-      book.isDisplayed = true;
-    }
-  });
-}
 
 function createCard(book) {
   const card = document.createElement('div');
@@ -88,41 +95,28 @@ function createRemoveButton(book, card, className) {
   const removeButton = document.createElement('button');
   removeButton.classList.add(className);
   removeButton.textContent = 'Remove Book';
-  removeButton.addEventListener('click', () =>
-    removeBookFromLibrary(card, book),
-  );
+  removeButton.addEventListener('click', () => {
+    library.removeBook(book);
+    bookContainer.removeChild(card);
+  });
   return removeButton;
 }
 
 function createStatusButton(book, className) {
   const statusButton = document.createElement('button');
   statusButton.classList.add(className);
-  statusButton.textContent = book.isRead ? 'Read' : 'Unread';
+  statusButton.textContent = book.status ? 'Read' : 'Unread';
   setButtonBackgroundColor(book, statusButton);
   statusButton.addEventListener('click', () => {
-    changeBookStatus(book, statusButton);
+    library.changeBookStatus(book);
+    statusButton.textContent = book.status ? 'Read' : 'Unread';
     setButtonBackgroundColor(book, statusButton);
   });
   return statusButton;
 }
 
-function removeBookFromLibrary(card, book) {
-  const bookIndex = library.indexOf(book);
-  library.splice(bookIndex, 1);
-  bookContainer.removeChild(card);
-}
-
-function changeBookStatus(book, btn) {
-  library.forEach((arrBook) => {
-    if (arrBook === book) {
-      arrBook.isRead ? (arrBook.isRead = false) : (arrBook.isRead = true);
-      btn.textContent = arrBook.isRead ? 'Read' : 'Unread';
-    }
-  });
-}
-
 function setButtonBackgroundColor(book, btn) {
-  btn.style.backgroundColor = book.isRead ? '#7fd1ae' : '#f48966';
+  btn.style.backgroundColor = book.status ? '#7fd1ae' : '#f48966';
 }
 
 // modal quit button
